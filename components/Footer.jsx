92a49cf37862369d4003649203df677bb4aa2a42
Mahmud Kaşgari <jan0613@gmail.com>
2022-07-25 20:15:09 +0300

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return <div className={styles.container}>
 
     <footer className={styles.footer}>
-      Made with ❤️, By  <strong>Mahmud Kaşgari</strong> , Open sourced on  <a href="https://github.com/Mahmud1985/myWebsite" target="_blank"><GitHubIcon /></a>
+      Made with ❤️, By  <strong>Mahmud Kaşgari</strong> , Open sourced on  <a href="https://github.com/Mahmud1985/myWebsite" target="_blank" rel="noopener"><GitHubIcon /></a>
     </footer>
   </div >
 };
